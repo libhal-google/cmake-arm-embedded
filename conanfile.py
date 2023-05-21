eaedf2887656d5c3d57ce3afeafad48ca1daf624
Khalil Estell <kammce@google.com>
2023-05-21 12:13:32 -0400

    :recycle: Remove toolchains in response to prebuilts
    This change removes the responsibility of providing minimal cmake
    toolchain files. Responsibility has been moved to the arm-gnu-toolchain
    package.
    
    This repo will now house helper cmake toolchain files such as making
    CMake the build type `RelWithDebInfo` actually on chip debuggable by
    using `-Og` rather than `-O2` as well as providing helper functions for
    generating .hex, .bin, and disassembly files.

--- conanfile.py
+++ conanfile.py
@@ -23,16 +23,24 @@ required_conan_version = ">=1.50.0"
 
 class CmakeArmEmbeddedConan(ConanFile):
     name = "cmake-arm-embedded"
-    version = "0.1.1"
+    version = "1.0.0"
     license = "Apache-2.0"
     url = "https://github.com/conan-io/conan-center-index"
     homepage = "https://libhal.github.io/libhal-armcortex"
     description = ("A collection of CMake scripts for ARM Cortex ")
-    topics = ("cmake", "ARM", "cortex", "cortex-m", "cortex-m0", "cortex-m0+",
+    topics = ("cmake", "arm", "cortex", "cortex-m", "cortex-m0", "cortex-m0+",
               "cortex-m1", "cortex-m3", "cortex-m4", "cortex-m4f", "cortex-m7",
               "cortex-m23", "cortex-m55", "cortex-m35p", "cortex-m33")
     exports_sources = ("cmake/*", "LICENSE")
     no_copy_source = True
+    options = {
+        "add_subroutines": [True, False],
+        "improve_debugging": [True, False]
+    }
+    default_options = {
+        "add_subroutines": True,
+        "improve_debugging": True
+    }
 
     def package_id(self):
         self.info.clear()
@@ -43,11 +51,22 @@ class CmakeArmEmbeddedConan(ConanFile):
     def package(self):
         copy(self, "LICENSE", dst=os.path.join(
             self.package_folder, "licenses"),  src=self.source_folder)
-        copy(self, "cmake/toolchain.cmake", src=self.source_folder,
+        copy(self, "cmake/*.cmake", src=self.source_folder,
              dst=self.package_folder)
 
     def package_info(self):
         # Add toolchain.cmake to user_toolchain configuration info to be used
         # by CMakeToolchain generator
-        f = os.path.join(self.package_folder, "cmake/toolchain.cmake")
-        self.conf_info.append("tools.cmake.cmaketoolchain:user_toolchain", f)
+        subroutines_path = os.path.join(
+            self.package_folder, "cmake/subroutines.cmake")
+        improve_debugging_path = os.path.join(
+            self.package_folder, "cmake/improve_debugging.cmake")
+
+        if self.options["add_subroutines"]:
+            self.conf_info.append(
+                "tools.cmake.cmaketoolchain:user_toolchain",
+                subroutines_path)
+
+        if self.options["improve_debugging"]:
+            self.conf_info.append(
+                "tools.cmake.cmaketoolchain:user_toolchain", improve_debugging_path)
